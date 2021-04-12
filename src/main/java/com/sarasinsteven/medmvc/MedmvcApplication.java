15eccbbe6139329a84ccab2229c28615c4c099d2
tutoringsteve <tutoringsteve@gmail.com>
2021-04-11 21:40:07 -0400

    Initial commit.

---
+++ src/main/java/com/sarasinsteven/medmvc/MedmvcApplication.java
@@ -0,0 +1,13 @@
+package com.sarasinsteven.medmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedmvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedmvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sarasinsteven/medmvc/MedmvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sarasinsteven.medmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedmvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
